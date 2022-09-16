108a47319bc3f0d91b5f261a4eb5f3344f30e12b
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-09-16 16:15:58 +0300

    numeric types

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 /* 
 
-let
-const
-var - застаріла
+numeric types 
 
 */
 
+let a = 3;
+let b = 5;
+let c;
+c = a + b;
+c = a - b;
+c = a * b;
+c = a / b;
+c = a / 0; // Infinity
+c = a / Infinity; //0
+c = 5 / 'a'; // NaN
+c = 'Hello' + 'World'; // 'HelloWorld' конкатинація рядків (завжди повертає рядок)
+c = 'Hello' * 'World'; // NaN
+c = 4 + 'hello'; // '4hello'
+c = null + ''; // 'null'
+c = true + ''; // 'true'
 
-let box; // декларація змінної
-let newBox = 3; // декларація + присвоювання
-box = 5; // присвоювання значення (ініціалізація - вперше щось поклали до змінної)
+с = '23' * 1; // 23
+с = '23' - 0; // 23
+c = Number('23') // 23
 
-box = true;
-box = 'Hello';
-console.log(box);
-
-let value = 'Very important value';
-
-let value1 = ''; // переменная, можна змінювати
-const a = 5; // константа, не можна змінювати
+Boolean('') // false
+Boolean(null) // false
+Boolean(undefined) // false
+Boolean('1') // true
+Boolean(2) // true
+Boolean({}) // true
 
 
+console.log(c);
