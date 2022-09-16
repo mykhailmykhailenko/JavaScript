b2bc9b92f2da6d8e22d73b6398c2622612cfec95
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-09-16 15:58:46 +0300

    let, const

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-console.log('Hello world');
-console.log(35);
-console.log(true);
-console.log(null);
-console.log(undefined);
-console.log(1n);
+/* 
+
+let
+const
+var - застаріла
+
+*/
+
+
+let box; // декларація змінної
+let newBox = 3; // декларація + присвоювання
+box = 5; // присвоювання значення (ініціалізація - вперше щось поклали до змінної)
+
+box = true;
+box = 'Hello';
+console.log(box);
+
+let value = 'Very important value';
+
+let value1 = ''; // переменная, можна змінювати
+const a = 5; // константа, не можна змінювати
+
+
