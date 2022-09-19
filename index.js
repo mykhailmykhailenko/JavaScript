a9fe27f7da1f063adf66472eef89ee0d7d098108
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-09-19 15:21:25 +0300

    if else task resolve

--- index.js
+++ index.js
@@ -1,53 +1,7 @@
-// 1. Створіть дві змінні - a i b. Виведіть у консоль результат їх множення
+let a = Number(prompt('Скільки вам років?'));
 
-// 2. Створіть дві змінні - c i d. Виведіть у консоль результат їх поділу
-
-// 2. Створіть дві змінні - e i f. Виведіть у консоль результат їх складання
-
-// 4. Визначіть змінні зі значеннями 33, false, 'javascript', 100. Виведіть їхні значення у консоль
-
-// 5. Переробіть зазначений нижче код так, щоб в ньому використовувались оператори +=, -=, *=, /=, ++, --
-
-// let num = 1;
-// num = num + 1;
-// num = num - 1;
-// num = num + 11;
-// num = num - 11;
-// num = num * 11;
-// num = num / 11;
-
-
-let a = 2;
-let b = 4;
-
-let c = 2;
-let d = 4;
-
-let e = 2;
-let f = 4;
-
-
-console.log(a*b);
-console.log(c/d);   
-console.log(e+f);
-
-
-let  g = 33;
-let i = false;
-let j = 'javascript';
-let h = 100;
-
-console.log(g);
-console.log(i);
-console.log(j);
-console.log(h);
-
-let num = 1;
-num += 1;
-num -= 1;
-num += 11;
-num -= 11;
-num *= 11;
-num /= 11;
-
-console.log(num);
+if (a >= 18)  {
+    console.log('Ви повнолітній!')
+} else {
+    console.log('Ви неповнолітній!')
+}
