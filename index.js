0b0239b6b0339ceb243798815232da0b0beb9137
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-09-19 16:57:28 +0300

    function для парних чи непарних чисел task resolve

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
-function multiplicateOfTwo (a, b) {
-    let result = a * b;
-    return result
-}
-
-let res = multiplicateOfTwo(2, 3);
-console.log(res)
-
-function theBiggerOfTwo (a, b) {
-    // debugger
-    if (a > b) {
-        console.log(a)
+function getNumber (a) {
+    if (a % 2 ) {
+        return false
     } else {
-        console.log(b)
+        return true
     }
 }
 
-theBiggerOfTwo (21, 68)
-
-function theBiggerOfTwo1 (a, b) {
-    // debugger
-    if (a > b) {
-        return a
-    } else {
-        return b
-    }
-}
-
-let number = theBiggerOfTwo1 (21, 68);
-console.log(number);
-
+let num = getNumber (99)
+console.log(num)
