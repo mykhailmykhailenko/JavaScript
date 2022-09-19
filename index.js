96ae8e9bb0270140941bd5dc638c815a2f285f8a
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-09-19 16:24:39 +0300

    function task resolve + debugger

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ function multiplicateOfTwo (a, b) {
 
 let res = multiplicateOfTwo(2, 3);
 console.log(res)
+
+function theBiggerOfTwo (a, b) {
+    // debugger
+    if (a > b) {
+        console.log(a)
+    } else {
+        console.log(b)
+    }
+}
+
+theBiggerOfTwo (21, 68)
+
+function theBiggerOfTwo1 (a, b) {
+    // debugger
+    if (a > b) {
+        return a
+    } else {
+        return b
+    }
+}
+
+let number = theBiggerOfTwo1 (21, 68);
+console.log(number);
+
