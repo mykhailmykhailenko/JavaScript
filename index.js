5f347f5e3d7e9079068849d6374c5c4df842ed79
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-09-20 10:38:44 +0300

    Logic operators

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
-function getNumber (a) {
-    if (a % 2 ) {
-        return false
-    } else {
-        return true
-    }
-}
+/* Логічні оператори */
 
-let num = getNumber (99)
-console.log(num)
+//!true = false
+// !false = true
+// !!true = true;
+// !!false = false;
+
+/// Порівняння
+// > - більше
+// < - менше
+// >= - більше або дорівнює
+// <= - менше або дорівнює
+
+// Логічне І (AND) - кон'юнкція
+true && true 
+true
+true && false
+false
+false && true
+false
+false && false
+false
+
+// Повертає перший false або останній true, якщо всі true
+
+const res = 2 && 'a' && {}; // {}
+const res2 = 1 && true && null;
+
+const res3 = 2+2 && 3+3 && 0 && 5;
+
+
+// Логічне АБО - диз'юнкція
+// повертає true, коли хоча б один операнд - true
+true || true
+true
+true || false
+true
+false || true
+true
+false || false
+false
+
+// перший true-операнд, якого зустріне, або останнє false
+
+
+const res5 = undefined || '' || 2;
+const res6 = '' || 1 || 0;
