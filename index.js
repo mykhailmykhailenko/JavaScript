2822b617546b262052f3e468225d2ed9de6a4044
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-09-22 16:37:25 +0300

    for in object task resolve

--- index.js
+++ index.js
@@ -1,16 +1,58 @@
-const weekPlan = {
-    Mon: 'dantist',
-    Tue: 'go to park',
-    Wed: 'meet friends',
-    Thu: 'learn',
-    Fri: 'hard work',
-    Sat: 'go to party'
-}
+// let user = {
+//     firstName: 'Alex',
+//     lastName: 'Doe',
+//     age: 20,
+//     email: 'test@mail.com'
+// }
 
-function showPlans () {
-    let askDay = prompt('Enter the day:')
-    return alert(weekPlan[askDay])
-}
+// for (let key in user) {
+//     console.log(key)
+// }
+// firstName
+// lastName
+// age
+// email
 
-showPlans()
+// for (let key in user) {
+//     console.log(user[key])
+// }
+// Alex
+// Doe
+// 20
+// est@mail.com
+
+// Exercise 1
+
+// let obj = {
+//     a: 1,
+//     b: 2,
+//     c: 5,
+//     d: 7,
+//     e: 9
+// }
+
+// let sum = 0;
+
+// for (let key in obj) {
+//     sum = obj[key] + sum
+// }
+// console.log(sum)
+
+// Exercise 2
+
+// let obj1 = {}
+// let obj2 = {
+//     a: 1
+// }
+
+// function checkObject (object) {
+//     for (let key in object) {
+//         if (key) {
+//             return false
+//         }
+//     } 
+//     return true
+// }
+
+
 
