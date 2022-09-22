bc0a4da7445d09741e097862c5e76d60a6790530
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-09-22 09:00:03 +0300

    Default arguments

--- index.js
+++ index.js
@@ -1,47 +1,28 @@
 /*
-Написати калькулятор
-сума
-різниця
-множення
-ділення
-Отримуємо від користувача за допомогою prompt 
-перше число 
-друге число
-символ дії - +, -, *, /
+const userAvatar = 'http://.....';
+const imagePlaceholder = './';
+let imageSrc = userAvatar ? userAvatar : imagePlaceholder;
+<img src="" />
 */
-let num1 = Number(prompt('Enter first num'));
-let num2 = Number(prompt('Enter second number'));
-let oper = prompt('Enter the operator');
 
-calculator(num1, num2, oper);
 
-function sum(a, b) {
-    return a + b;
+/*
+Спитайте у юзера, як його звуть, і виведіть на консоль "Hello " + userName
+Якщо користувач нічого не ввів - виведіть "Hello, Anonym"
+*/
+
+const userName = prompt('What is your name?');
+
+function sayHello(name = 'Anonym'){
+    console.log('Hello ' + name);
 }
 
-function sub(a, b) {
-    return a - b;
-}
 
-function multy(a, b) {
-    return a * b;
-}
+sayHello('Alex');
 
-function div(a, b){
-    return a / b;
-}
 
-function calculator(firstNum, secondNum, operator) {
-    switch(operator) {
-        case '+':
-            return sum(firstNum, secondNum);
-        case '-':   
-            return sub(firstNum, secondNum);
-        case '*':
-            return multy(firstNum, secondNum);
-        case '/':
-            return div(firstNum, secondNum);
-        default: 
-            return 'Unexpected operation'
-    }
-}
+sayHello('John');
+
+sayHello(userName);
+
+sayHello();
