b4a96a66c5dd6e3d3f04dea10533e7822a932283
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-09-22 11:27:33 +0300

    FizzBuzz task resolve

--- index.js
+++ index.js
@@ -1,92 +1,14 @@
-// Калькулятор
-
-// let num1 = Number(prompt('Введите первое целое число'));
-// let num2 = Number(prompt('Введите второе целое число'));
-// let whatToDo = prompt('Введите знак десйствия');
-// let sum;
-
-// switch (whatToDo) {
-//     case '+' : sum = num1 + num2;
-//     break
-//     case '-' : sum = num1 - num2;
-//     break
-//     case '*' : sum = num1 * num2;
-//     break
-//     case '/' : sum = num1 / num2;
-//     break
-// }
-
-// console.log(sum)
-
-
-
-// let imageSrc = userAvatar ? userAvatar : imagePlaceholder;
-
-// let userName = prompt('Як вас звати?')
-
-// if (userName)  {
-//     console.log('Hello ' + userName);
-// } else {
-//     console.log('Hello anonim');
-// }
-
-
-
-// let correctPassword = 'GoodPassword';
-// let userPassword = '';
-
-// do {
-//     userPassword = prompt('Введите пароль:');
-// } while (userPassword !== correctPassword)
-
-// console.log('Пароль правильний');
-
-// let i = 0;
-
-// while (i < 10) {
-//     i++
-//     if (i % 2) {
-//         console.log(i);
-//     } else {
-
-//     }
-// }
-
-
-
-// let k = 0;
-// let sum = 0;
-
-// while (k++ < 5) {
-//     sum = sum + k;
-// }
-
-// console.log(sum);
-
-
-
-// function factorial (number) {
-//     let sum = 1;
-//     for (let i = 1; i <= number; i++) {
-//         sum = sum * i;
-//     }
-//     return sum;
-// }
-
-// function pow (base, power) {
-//     let sum = 1;
-//     for (let i = 1; i <= power; i++) {
-//         sum = sum * base;
-//     }
-//     return sum
-// }
-
-let sum = 0;
-function findDivider (number) {
-    for (let i = 0; i <= number; i++ ) {
-       if (number % i === 0) {
-            console.log(i)
-       }
+function calculate (number) {
+    for (let i = 0; i <= number; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('FizzBazz');
+        } else if (i % 5 === 0) {
+            console.log('Bazz'); 
+        } else if (i % 3 === 0) {
+            console.log('Fizz'); 
+        } else {
+            console.log(i);
+        }
     }
 }
-
+    
