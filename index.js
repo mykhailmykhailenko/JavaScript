39af27217c547ef208c41b5afdf6437b1111bad8
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-09-22 08:53:26 +0300

    for цикл

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-let a = 1;
+// for (ініціалізація ; умова ; інкрементація; ) {
+//     блок коду
+// }
 
-while (a++ < 10) {
-    if (a % 2 === 0) {
-        console.log(a)
-    } 
+// for (let a = 0; a <= 10; a++) {
+//     console.log(a)
+// }
+
+
+// function areaRectangle (width, height) {
+//     let result = width * height
+//     return result
+// }
+
+// areaRectangle (5, 10)
+
+// function circleLength (diameter) {
+//     const PI = 3.14;
+//     let result = PI * diameter
+//     return result
+// }
+
+// circleLength (5)
+
+// for (let i = 1; i <=100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log('FizzBuzz')
+//     } else if (i % 5 === 0) {
+//         console.log('Buzz')
+//     } else if (i % 3 === 0) {
+//         console.log('Fizz')
+//     } else {
+//         console.log(i)
+//     }
+// }
+
+function calculate (number) {
+    for (let i = 1; i <= number; i++) {
+    console.log(number / i)
+    }
+    
 }
