41d73a8773e2ef3d7afa2b41c3516833fdda1c0e
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-09-22 08:59:15 +0300

    Calculator task resolve

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
-function square(b) {
-    return b*b;
+/*
+Написати калькулятор
+сума
+різниця
+множення
+ділення
+Отримуємо від користувача за допомогою prompt 
+перше число 
+друге число
+символ дії - +, -, *, /
+*/
+let num1 = Number(prompt('Enter first num'));
+let num2 = Number(prompt('Enter second number'));
+let oper = prompt('Enter the operator');
+
+calculator(num1, num2, oper);
+
+function sum(a, b) {
+    return a + b;
 }
 
-
-function superFunction(number, fn) { // функція вищого порядку (High Order Function - HOF)
-    // отут може бути купа роботи самої функції superFunction
-    fn(number); // callback - функція зворотнього виклику
-
+function sub(a, b) {
+    return a - b;
 }
 
-superFunction(5, square);
+function multy(a, b) {
+    return a * b;
+}
+
+function div(a, b){
+    return a / b;
+}
+
+function calculator(firstNum, secondNum, operator) {
+    switch(operator) {
+        case '+':
+            return sum(firstNum, secondNum);
+        case '-':   
+            return sub(firstNum, secondNum);
+        case '*':
+            return multy(firstNum, secondNum);
+        case '/':
+            return div(firstNum, secondNum);
+        default: 
+            return 'Unexpected operation'
+    }
+}
