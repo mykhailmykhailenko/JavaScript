7b8769a23676a5cb87feb094ca22301c32e32c03
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-09-26 11:56:12 +0300

    Template String in work

--- index.js
+++ index.js
@@ -1,25 +1,58 @@
-/*
-Функція-конструктор робітника (Worker). Робітник має ім'я, прізвище, ставку за день (rate), кількість відпрацьованих днів в цьому місяці.
-Метод:
-getSalary - має розрахувати, скільки робітник отримає зп в цьому місяці, засн. на ставці та кількості відпрацьованих днів.
-*/
+this.currentSpeed = 0;
 
-function Worker(firstName, lastName, rate, days) {
-this.firstName = firstName;
-this.lastName = lastName;
-if(rate < 0) {
-this.rate = 0;
-} else {
-this.rate = rate; 
-}
-if (days > 31) {
-this.days = 31; 
-} else {
-this.days = days;
-}
-this.getSalary = function() {
-return this.rate * this.days;
+    this.run = function () {
+        // return this.model + ' is running with ' + this.currentSpeed + ' km/h';
+        return `${this.model} is running with ${this.currentSpeed} km/h`;
+    }
+    this.stop = function() {
+        this.currentSpeed = 0;
+	
+    this.firstName = firstName;
+    this.lastName = lastName;
+    if(rate < 0) {
+       this.rate = 0;
+    } else {
+        this.rate = rate; 
+    }
+    return this; // primitive will be ignored, this will be returned
+ }
+
+const wrkr = new Worker('Black', 'Widow', 100500, 10);
+
+ /*
+Шаблонні рядки
+ */
+
+const str = 'say: \n "hello"'; 
+//alert('2234234234+2234234234='+(2234234234+2234234234));
+
+const templateString = `2+2
+${wrkr.getSalary()}
+=4`;
+
+console.log(templateString);
+
+
+
+//////////////////
+
+const protocol = 'http';
+const domain = 'www.example.com';
+const port = 80;
+const path = 'path/to/myfile.html';
+
+const query = {
+    key1: 'value1',
+    key2: 'value2',
+    key3: 'value3'
 }
 
-return this; // primitive will be ignored, this will be returned
+
+function createURL(protocol, domain, port, path, queryParams) {
+    let url = `${protocol}://${domain}:${port}/${path}?`;
+    for (const key in queryParams) {
+       url += `${key}=${queryParams[key]}&`;
+    }
+
+    return url;
 }
