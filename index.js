38a0ada884b76c171bc268120f6a41fdcf1952a1
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-09-26 11:51:19 +0300

    this

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-const a = {
-    key: 'value',
-    key2: 'value2'
-}
+'use strict'
 
-const b = {
-    key: 'value',
-    key2: 'value2'
-}
-
-const c = {
-    key: 123,
-    key2: 'value2'
-}
-
-
-function checkObject (object1, object2) {
-   for (let i in object1) {
-    for (let j in object2) {
-        if (object1[i] === object2[j]) {
-            return true
-        } else {
-            return false
-        }
+const user1 = {
+    firstName: 'Alex',
+    lastName: 'Grow',
+    age: 15,
+    getName: function () {
+        console.log(this.firstName + this.lastName);
     }
-   }
+}
+
+
+const user2 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 15,
+    getName: function () {
+        console.log(this.firstName + this.lastName);
+    }
 }
