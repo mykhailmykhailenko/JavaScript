6130ce4967bccbf724aa3bb5d9ebfb3c0342f9c2
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-09-26 16:22:50 +0300

    array methods

--- index.js
+++ index.js
@@ -1,20 +1,95 @@
 'use strict'
 
-const array =  [1,2,3,4,5,6,7,8,9,10]
+// const array =  [1,2,3,4,5,6,7,8,9,10]
+
+//array.push(11,12) // add in the end
+
+//array.unshift(-1) // add in the start
+
+//array.shift() // видаляє перший єлемент масива і повертає його значення
+
+//array.pop() // видаляє останній єлемент масива і повертає його значення
+
+// array.join() 
+// array.join('')
+// array.join(' ')
+// array.join('*') // склеює елементи массива
 
 
-// function calculate (arr) {
-//     return arr[2] + arr[4];
-// }
-// calculate (arr);
+// const arr1 = [1,2,3];
+// const arr2 = [4,5,6];
+// const concatArrays = arr1.concat(arr2) // повертає новий массив, склеюючи два масиви
 
-// среднее арифметическое массива
+// const newArr = new Array(10) // 10 пустих елементів
+// newArr.fill(null) // заповнює масив всі єлементи одним значенням null
 
-// function sumOfArray(array) {
-//     let sum = 0;
-//     for (let i = 0; i < array.length; i++) {
-//         sum += array[i]
-//     }
-//     return sum / array.length
-// }
+// array.includes(2) // чи містить наш массив елемент 2, повертає true або false
+
+// array.indexOf(3) // повертає індекс першого знайденого такого елементу, або -1 якщо елементу немає
+
+// array.slice(1,4) // вирізає частину массиву за вказаними індексами, повертає новий массив
+// array.slice(3) // з третього елементу до кінця
+
+// array.splice(2, 3) // починаючи с другого елементу видаляє вказану кількусть елементів, повертає массив з видалених елементів, сзмінив початковий массив
+// array.splice(1, 1, 22) // видалили
+
+
+
+// TASK RESOLVE
+
+// 1
+// const arr1 = [3, 4, 5];
+// const arr2 = [10, 12, 13];
+// const array = arr1.concat(arr2)
+// console.log(array)
+
+//2
+// const arr = [1,2,3];
+// arr.reverse()
+// console.log(arr)
+
+//3
+// const arr = [3,4,5];
+// arr.push(7,8,9)
+// console.log(arr)
+
+//4
+// const arr = [3,4,5];
+// arr.unshift(1,2)
+// console.log(arr)
+
+//5
+// const arr =['aaa', 'bbb', 'ccc']
+// console.log(arr.pop())
+// console.log(arr)
+
+//6
+// const arr =['aaa', 'ddd', 'eee']
+// console.log(arr.shift())
+// console.log(arr)
+
+//7
+// const arr = [1,2,3,4,5,6,7,8,9]
+// const arr2 = arr.slice(3,5)
+// console.log(arr2)
+
+//8
+// const arr = [1,2,3,4,5,6,7,8,9]
+// arr.splice(1, 2)
+// arr.splice(3)
+// console.log(arr)
+
+//9 
+// const arr = [1,2,3,4,5];
+// arr.splice(1,0,'w','tr')
+// arr.splice(6,0,'vvv')
+// console.log(arr)
+
+//10
+// const arr = ['a', 'b', 'c', 'd']
+// const arr1 = arr.reverse()
+// const arr2 = arr1.join('')
+// console.log(arr2)
+
+
 
