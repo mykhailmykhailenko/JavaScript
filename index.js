7573aaa89e82eb20b064b98173e1207ab7dc1c7b
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-09-26 15:04:09 +0300

    array task resolve

--- index.js
+++ index.js
@@ -1,58 +1,20 @@
-this.currentSpeed = 0;
+'use strict'
 
-    this.run = function () {
-        // return this.model + ' is running with ' + this.currentSpeed + ' km/h';
-        return `${this.model} is running with ${this.currentSpeed} km/h`;
-    }
-    this.stop = function() {
-        this.currentSpeed = 0;
-	
-    this.firstName = firstName;
-    this.lastName = lastName;
-    if(rate < 0) {
-       this.rate = 0;
-    } else {
-        this.rate = rate; 
-    }
-    return this; // primitive will be ignored, this will be returned
- }
-
-const wrkr = new Worker('Black', 'Widow', 100500, 10);
-
- /*
-Шаблонні рядки
- */
-
-const str = 'say: \n "hello"'; 
-//alert('2234234234+2234234234='+(2234234234+2234234234));
-
-const templateString = `2+2
-${wrkr.getSalary()}
-=4`;
-
-console.log(templateString);
+const array =  [1,2,3,4,5,6,7,8,9,10]
 
 
+// function calculate (arr) {
+//     return arr[2] + arr[4];
+// }
+// calculate (arr);
 
-//////////////////
+// среднее арифметическое массива
 
-const protocol = 'http';
-const domain = 'www.example.com';
-const port = 80;
-const path = 'path/to/myfile.html';
+// function sumOfArray(array) {
+//     let sum = 0;
+//     for (let i = 0; i < array.length; i++) {
+//         sum += array[i]
+//     }
+//     return sum / array.length
+// }
 
-const query = {
-    key1: 'value1',
-    key2: 'value2',
-    key3: 'value3'
-}
-
-
-function createURL(protocol, domain, port, path, queryParams) {
-    let url = `${protocol}://${domain}:${port}/${path}?`;
-    for (const key in queryParams) {
-       url += `${key}=${queryParams[key]}&`;
-    }
-
-    return url;
-}
