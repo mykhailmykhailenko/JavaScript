97deb9d738925b4f7b8f679ad6a8fa8a9e597d1b
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-09-28 12:15:05 +0300

    MyArray.push for all parameters

--- Myarray.js
+++ Myarray.js
@@ -3,9 +3,17 @@
 function MyArray () {
     this.length = 0;
 
-    this.push = function(value) {
-        this[this.length] = value;
-        return ++this.length;
+    // this.push = function(value) {
+    //     this[this.length] = value;
+    //     return ++this.length;
+    // }
+
+    this.push = function() {
+        for (let i = 0; i < arguments.length; i++) {
+            this[this.length] = arguments[i];
+            this.length++;
+        }
+        return this.length;
     }
 
     this.pop = function() {
