c473767905614038a5da76e390ae557390645814
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-09-29 17:41:44 +0300

    arrowFunc arguments

--- index.js
+++ index.js
@@ -1,67 +1,18 @@
-'use strict'
-
-// const obj = {
-//     key: 'value',
-//     testMethod: test
-// }
+ 'use strict'
 
 
 
-
-// const test2 = function () {
-//     console.log(this);
-// }
-
-// const arrowFunction = () => {
-//     console.log(this);
-// }
+ function sumAllArguments() {
+    console.log(arguments)
+ }
 
 
-// // obj.testMethod();
-// // test();
-// // test2();
-// // arrowFunction();
+ const sum2 = () => {
+    console.log(arguments); // ARROW FUNCTION HAS NOT ARGUMENTS
+ }
 
 
-// // console.log(this);
+ sumAllArguments(3, 2, 1, 2, 3, 4);
 
 
-const newspaper = {
-    title: 'Super Yellow Press',
-    articles: [
-        {
-            author: 'John Doe',
-            text: 'Lorem ipsum',
-            date: 'today'
-        },
-        {
-            author: 'Jane Doe',
-            text: 'Dollor sit amet',
-            date: 'today'
-        },
-        {
-            author: 'Alex Doe',
-            text: 'Today is the day',
-            date: 'today'
-        },
-        {
-            author: 'Sandy Doe',
-            text: 'Lorem ipsum',
-            date: 'today'
-        }
-    ],
-    showArticles: function () {
-        this.articles.forEach((function (article) {
-            console.log(`${this.title}. ${article.author}: ${article.text}`);
-        }).bind(this));
-    }
-}
-
-function test() {
-    console.log(this);
-}
-
-
-const newTestFunction = test.bind(newspaper);
-newTestFunction();
-test();
+ sum2(3, 2, 1, 2, 3, 4)
