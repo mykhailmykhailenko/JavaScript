06947f2851b48bee3b2a03da84ec67ba260a998b
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-09-29 17:41:10 +0300

    .bind()

--- index.js
+++ index.js
@@ -1,8 +1,60 @@
 'use strict'
 
-const obj = {
-    key: 'value',
-    testMethod: test
+// const obj = {
+//     key: 'value',
+//     testMethod: test
+// }
+
+
+
+
+// const test2 = function () {
+//     console.log(this);
+// }
+
+// const arrowFunction = () => {
+//     console.log(this);
+// }
+
+
+// // obj.testMethod();
+// // test();
+// // test2();
+// // arrowFunction();
+
+
+// // console.log(this);
+
+
+const newspaper = {
+    title: 'Super Yellow Press',
+    articles: [
+        {
+            author: 'John Doe',
+            text: 'Lorem ipsum',
+            date: 'today'
+        },
+        {
+            author: 'Jane Doe',
+            text: 'Dollor sit amet',
+            date: 'today'
+        },
+        {
+            author: 'Alex Doe',
+            text: 'Today is the day',
+            date: 'today'
+        },
+        {
+            author: 'Sandy Doe',
+            text: 'Lorem ipsum',
+            date: 'today'
+        }
+    ],
+    showArticles: function () {
+        this.articles.forEach((function (article) {
+            console.log(`${this.title}. ${article.author}: ${article.text}`);
+        }).bind(this));
+    }
 }
 
 function test() {
@@ -10,17 +62,6 @@ function test() {
 }
 
 
-const test2 = function () {
-    console.log(this);
-}
-
-const arrowFunction = a => a*a;
-
-
-obj.testMethod();
+const newTestFunction = test.bind(newspaper);
+newTestFunction();
 test();
-test2();
-arrowFunction();
-
-
-console.log(this);
