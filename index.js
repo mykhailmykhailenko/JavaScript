8f274d0a1f915eb8506fa4591257509528811b50
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-10-04 14:47:12 +0300

    try {} cath {} errors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+
 function pow (base, power) {
     let res = 1;
     if (typeof base !== 'number' || typeof power !== 'number') {
@@ -13,3 +14,14 @@ function pow (base, power) {
     }
     return res
 }
+
+
+
+try {
+    console.log('start');
+    pow (2, -2);
+    console.log('end');
+} catch(error) {
+    console.log(error);
+}
+console.log('our code is alive')
