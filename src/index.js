c7d91f1c8cd99c6380bf07f241dfc3e58862f4e1
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-10-05 09:55:19 +0300

    Stack illustration

--- src/index.js
+++ src/index.js
@@ -1,39 +1,21 @@
 'use strict'
 
-
- class Worker {
-    constructor(name, rate, days) {
-        this.name = name;
-        this.rate = rate;
-        this.days = days;
-    }
-
-    set rate(value){
-        if(value < 0 ) {
-            throw new RangeError('rate must be > 0');
-        }
-        this._rate = value;
-    }
-
-    get rate() {
-        return this._rate;
-    }
+function doSomeStuff(a, b) {
+       return a+square(b);
+}
 
 
-    set days(value) {
-        if(typeof value !== 'number') {
-            throw new TypeError('Days must be a number');
-        }
-
-        this._days = value;
-    }
-
-    get days() {
-        return this._days;
-    }
+function square(b) {
+       return b*b;
+}
 
 
-    getSalary() {
-        return this.rate*this.days;
-    }
- }
+function consoleResult(){
+       debugger;
+       console.log(doSomeStuff(2,2))
+}
+
+consoleResult();
+
+
+console.log('End of script')
