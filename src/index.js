47dd88b118a3b81a2d738dc28a0e3d71e95457cc
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-10-05 23:48:06 +0300

    User+Moderator Hometask

--- src/index.js
+++ src/index.js
@@ -1,45 +1,29 @@
-'use strict'
-
-class Vallidator {
-
-  constructor (from, to) {
-    this.from = from;
-    this.to = to;
-  }
-  
-  set from(value) {
-    if(typeof value !== 'number') {
-      throw new TypeError('from must be a number')
+class User {
+    constructor(mail, password) {
+        this.mail = mail;
+        this.password = password;
+        this.isBanned = false;
     }
-    this._from = value;
-  }
-  get from() {
-    return this._from
-  }
 
-  set to(value) {
-    if(typeof value !== 'number') {
-      throw new TypeError('to must be a number')
+    sayHello () {
+        return `${this.mail} says: 'Hello!'`
     }
-    this._to = value;
-  }
-  get to() {
-    return this._to
-  }
-  
-  get range() {
-      let arr = [];
-      for (let i = this._from; i <= this._to; i++) {
-        arr.push(i)
-      }
-      return arr
-    }
-    
-  checkNumber (number) {
-    return this.range.includes(number)
-  }
-
 
 }
 
-const test1 = new Vallidator(2, 5);
+class Moderator extends User {
+    constructor(mail, password) {
+        super(mail, password)
+    }
+
+    giveBanned (obj) {
+        return obj.isBanned = true;
+    }
+}
+
+const user1 = new User('misha@ukr.net', '123321')
+user1.sayHello()
+
+const moderatorMain = new Moderator('moderatorMain@ukr.net', '123321');
+
+moderatorMain.giveBanned(user1)
