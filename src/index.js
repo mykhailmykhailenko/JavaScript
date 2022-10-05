9e48b66d1409a090845a6bd5d81d433f7fe7716c
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-10-05 15:15:47 +0300

    get, set task resolve

--- src/index.js
+++ src/index.js
@@ -1,11 +1,45 @@
 'use strict'
 
-/**
-  * 
-  * @param {String} name - Username to sayHello to user
-  * @returns {undefined}
-  */
+class Vallidator {
 
- function sayHello(name) {
-    console.log(`Hello, ${name}`);
+  constructor (from, to) {
+    this.from = from;
+    this.to = to;
+  }
+  
+  set from(value) {
+    if(typeof value !== 'number') {
+      throw new TypeError('from must be a number')
+    }
+    this._from = value;
+  }
+  get from() {
+    return this._from
+  }
+
+  set to(value) {
+    if(typeof value !== 'number') {
+      throw new TypeError('to must be a number')
+    }
+    this._to = value;
+  }
+  get to() {
+    return this._to
+  }
+  
+  get range() {
+      let arr = [];
+      for (let i = this._from; i <= this._to; i++) {
+        arr.push(i)
+      }
+      return arr
+    }
+    
+  checkNumber (number) {
+    return this.range.includes(number)
+  }
+
+
 }
+
+const test1 = new Vallidator(2, 5);
