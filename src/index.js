fc03b718eee82fea47c3932a73c259ad11dc1870
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-10-05 09:56:59 +0300

    SumOfArrays

--- src/index.js
+++ src/index.js
@@ -1,30 +1,34 @@
 'use strict'
 
 
-/* 
-Задача - написати рекурсивну функцію, яка обчислює факторіал числа
-5! = 1*2*3*4*5
-5! = 4! * 5 = 3! * 4 * 5
-*/
+// const pow2 = (base, power) => (power === 1) ? base : pow2(base, power - 1);
 
-
-function factorial(number) {
-       debugger;
-       if(number === 1) {
-               return 1;
-       }
-       return number * factorial (number - 1);
-}
 
 /*
-Написати функцію, яка зводить у ступінь передане їй число
-pow(base, power)
+Дан масив: [2, 3, 2, 1, [4, 2, 3, [6, 3,2, [3], 3, 3], 2]]
+Задача - порахувати суму всіх елементів масива і вкладених масивів
+Не використовувати методи масивів
 */
 
+const arr = [2, 3, 2, 1, [4, 2, 3, [6, 3,2, [3], 3, 3], 2]];
 
-function pow(base, power) {
-       if(power === 1) {
-               return base
-       }
-       return base * pow(base, power-1);
+function sumOfArrays(array){
+        let sum = 0;
+        for (let i=0; i< array.length; i++) {
+                if(Array.isArray(array[i])) {
+                        sum += sumOfArrays(array[i]);
+                } else {
+                         sum += array[i];
+                }
+        }
+
+        return sum;
 }
+
+
+console.log(sumOfArrays(arr));
+
+
+const arr2 = [5, 1, 3, 4, [5, 3, 2, [2, 10]]];
+
+console.log(sumOfArrays(arr2));
