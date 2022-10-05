45067b14b9263eb80874244c5b4fd64e71759333
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-10-05 09:58:18 +0300

    Fibonacchi task (resolve, but not ideal)

--- src/index.js
+++ src/index.js
@@ -1,34 +1,24 @@
 'use strict'
 
 
-// const pow2 = (base, power) => (power === 1) ? base : pow2(base, power - 1);
-
-
 /*
-Дан масив: [2, 3, 2, 1, [4, 2, 3, [6, 3,2, [3], 3, 3], 2]]
-Задача - порахувати суму всіх елементів масива і вкладених масивів
-Не використовувати методи масивів
+Вивести на консоль перші 100 членів ряду Фібоначчі
+F(n) = F(n-1) + F(n-2)
+F(1) = 1
+F(2) = 1
 */
 
-const arr = [2, 3, 2, 1, [4, 2, 3, [6, 3,2, [3], 3, 3], 2]];
 
-function sumOfArrays(array){
-        let sum = 0;
-        for (let i=0; i< array.length; i++) {
-                if(Array.isArray(array[i])) {
-                        sum += sumOfArrays(array[i]);
-                } else {
-                         sum += array[i];
-                }
-        }
-
-        return sum;
+function fn(n) {
+    if(n === 0 ) {
+            return 0;
+    }
+    if(n===2 || n===1) {
+            return 1
+    }
+    return fn(n-1) + fn(n-2)
 }
 
-
-console.log(sumOfArrays(arr));
-
-
-const arr2 = [5, 1, 3, 4, [5, 3, 2, [2, 10]]];
-
-console.log(sumOfArrays(arr2));
+for (let i = 0; i<10; i++) {
+    console.log(fn(i));
+}
