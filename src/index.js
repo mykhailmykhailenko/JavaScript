06183346730ad201fb031be8b1ac88ebd8ad4449
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-10-05 10:00:07 +0300

    JSDoc task resolve

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 'use strict'
 
 /**
- * 
- * @param {Number} a - First param of function 
- * @param {Number} b - Second param of function
- * @returns {Number} - Sum of two numbers
- */
+  * 
+  * @param {String} name - Username to sayHello to user
+  * @returns {undefined}
+  */
 
-function sum(a, b) {
-       return a+b;
+ function sayHello(name) {
+    console.log(`Hello, ${name}`);
 }
-
-sum(4, 5)
