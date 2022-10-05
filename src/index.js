2be23959943fb87ee68d6e48a29559167dd3bbca
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-10-05 09:59:31 +0300

    JSDoc example

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
 'use strict'
 
+/**
+ * 
+ * @param {Number} a - First param of function 
+ * @param {Number} b - Second param of function
+ * @returns {Number} - Sum of two numbers
+ */
 
-/*
-Вивести на консоль перші 100 членів ряду Фібоначчі
-F(n) = F(n-1) + F(n-2)
-F(1) = 1
-F(2) = 1
-*/
-
-
-function fn(n) {
-    if(n === 0 ) {
-            return 0;
-    }
-    if(n===2 || n===1) {
-            return 1
-    }
-    return fn(n-1) + fn(n-2)
+function sum(a, b) {
+       return a+b;
 }
 
-for (let i = 0; i<10; i++) {
-    console.log(fn(i));
-}
+sum(4, 5)
