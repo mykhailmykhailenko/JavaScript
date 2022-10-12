79d772c101d3bbf86924a81be46f8e1c097af77b
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-10-12 08:20:59 +0300

    MyNewArr (reverse, flat, concat)

--- src/new_syntax.js
+++ src/new_syntax.js
@@ -23,19 +23,52 @@ class MyNewArray {
     }
 
     map(callback) {
-        const arr = new MyArray();
+        const arr = new MyNewArray();
         for (let i = 0; i < this.length; i++){
             arr.push(callback(this[i]))
         }
         return arr
     }
+
     static isMyNewArray(obj) {
         return obj instanceof MyNewArray;
     }
-    
+
+    concat (arr2) {
+        for (let i = 0; i < arr2.length; i++) {
+            this.push(arr2[i])
+        }
+        return this
+    }
+
+    flat() {
+        const res = new MyNewArray();
+        for (let i = 0; i < this.length; i++) {
+            if (MyNewArray.isMyNewArray(this[i])) {
+                res.concat(this[i].flat());
+            } else {
+                res.push(this[i]);
+            }
+        } 
+        return res
+    }
+
+    reverse() {
+       const len = this.length - 1;
+        for (let i = 0; i <= len / 2; i++) {
+            let tmp = this[i];
+            this[i] = this[len - i];
+            this[len - i] = tmp;
+        }
+        return this;
+    }
 }
+
+
 
 
 const myarr = new MyNewArray();
 
 MyNewArray.isMyNewArray(myarr) //true
+
+
