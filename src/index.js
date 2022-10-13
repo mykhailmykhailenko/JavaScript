afecca0fb1876b628a551fbd7870f1828ef93741
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-10-13 19:23:35 +0300

    BinarySearch

--- src/index.js
+++ src/index.js
@@ -1,79 +1,22 @@
 'use strict'
 
-/*
-Напишіть клас Student який має властивості - 
-ім'я
-прізвище
-рік вступу до університету (число! 2017)
-метод getCourse, який вираховує, на якому курсі вчиться студент (поточний рік отримайте від Date)
-*/
+const arr = [2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+function binarySearchFunction(array, searchValue) {
+    let start = 0;
+    let end = array.length-1;
+    let middle = Math.round((start+end)/2);
 
-class Student{
-    constructor(firstName, lastName, yearOfGrad) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.yearOfGrad = yearOfGrad; 
-    }
-
-    getCourse() {
-        return new Date().getFullYear - this.yearOfGrad;
-    }
-}
-
-
-
-
-
-/*
-Написати функцію, яка повертає рядок з назвою поточної пори року.
-'зима' - якщо на дворі з грудня по січень,
-'осінь' - з вересня по листопад
-'літо' - з червня по серпень
-'весна' - з березня по травень
-*/
-
-function getSeason(){
-    const month = new Date().getMonth();
-    // if(month <=1 || month === 11) {
-    //     return 'winter'
-    // } else if(month >= 2 && month <=4) {
-    //     return 'spring'
-    // } else if(month >= 5 && month <= 7) {
-    //     return 'summer'
-    // } else {
-    //     return 'fall'
-    // }
-
-    switch(month){
-        case 0:
-        case 1:
-        case 11:
-            return 'winter';
-        case 2:
-        case 3:
-        case 4:
-            return 'spring';
-        case 5: 
-        case 6:
-        case 7:
-            return 'summer';
-        case 8:
-        case 9:
-        case 10:
-            return 'fall';
-        default: 
-            return null;
+    while(true) {
+        if(searchValue === array[middle]) {
+            return middle;
+        }
+        if (searchValue > array[middle]){
+            start = middle + 1;
+            middle = Math.floor((start+end)/2)
+        } else {
+            end = middle - 1;
+            middle = Math.floor((start+end)/2);
+        }
     }
 }
-
-
-
-/*
-Вивести на екран поточну дату у форматі:
-"день-місяць-рік"
-(7-10-2022)
-*/
-
-const date = new Date();
-console.log(`${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`);
