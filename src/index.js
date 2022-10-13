b913114a00bd5069b001c3ad94e15d6d07539d35
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-10-13 18:08:55 +0300

    LinkedList Iterator and delete methods

--- src/index.js
+++ src/index.js
@@ -1,35 +1,37 @@
 'use strict'
 
 class ListItem {
-    constructor(value){
+    constructor (value) {
         this.value = value;
         this.next = null;
         this.prev = null;
     }
 
-    get value() {
-        this._value
+    get () {
+        return this._value;
     }
 
-    set value(v) {
+    set (v) {
         this._value = v;
     }
 }
 
-
 class LinkedList {
-    constructor(...args){
+    constructor(...args) {
         this.length = 0;
         this.head = null;
         this.tail = null;
 
         for (const item of args) {
-            this.push(item);
-        }
+            this.push(item)
+        } 
     }
 
-    push(v) {
+   
+
+    push (v) {
         const newItem = new ListItem(v);
+
         if (this.length === 0) {
             this.head = newItem;
             this.tail = newItem;
@@ -38,10 +40,56 @@ class LinkedList {
             newItem.prev = this.tail;
             this.tail = newItem;
         }
+
         return ++this.length
+    }
+
+    deleteHeadElement () {
+        const nextElement = this.head.next;
+        nextElement.prev = null;
+        this.head = nextElement;
+        this.length--;
+    }
+
+
+    deleteTailElement() {
+            const prevElement = this.tail.prev;
+            prevElement.next = null;
+            this.tail = prevElement;
+            this.length--;
+    }
+
+    deleteElement(value) {
+        for (const item of this) {
+            if(item.value === value) {
+                const nextElement = item.next;
+                const prevElement = item.prev;
+                nextElement.prev = prevElement;
+                prevElement.next = nextElement;
+            }
+        }
+        this.length--;
+
+    }
+
+     
+
+    [Symbol.iterator] () {
+        return new LinkedListIterator(this)
     }
 }
 
-/*
-Реалізувати додавання будь-якої кількості елементів у зв'язаний список при створенні його.
-*/
+class LinkedListIterator {
+    constructor(list) {
+        this.list = list;
+        this.currentNode = null;
+    }
+
+    next () {
+        this.currentNode = this.currentNode ? this.currentNode.next : this.list.head;
+        return {
+            value: this.currentNode,
+            done: !this.currentNode,
+        }
+    }
+}
