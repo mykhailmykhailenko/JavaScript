d8c6125bd5b8221d5c050b695c2ed7ef63d3d8e1
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-10-16 19:52:39 +0300

    Queue merge task resolve

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 'use strict'
 
 class Queue {
-    constructor(){
+    constructor(...args){
         this._head = 0;
         this._tail = 0; 
+
+        for (const value of args) {
+            this.enqueue(value);
+        }
     }
 
 
@@ -27,6 +31,30 @@ class Queue {
             return firstItem;
         }
     }
+}
+
+/*
+Написати фунцію mergeQueues(q1, q2), яка послідовно об'єднує (зливає) дві черги в одну.
+1, 2, 3
+5, 6, 7
+1, 5, 2, 6, 3, 7
+*/
+
+
+function mergeQueues(q1, q2) {
+    const resQueue = new Queue();
+    const maxLength = q1.size > q2.size ? q1 : q2;
+    while(maxLength.size){
+        if(q1.size){
+            resQueue.enqueue(q1.dequeue())
+        }
+        if(q2.size){
+        resQueue.enqueue(q2.dequeue());
+        }
+    }
+
+
+    return resQueue;
 }
 
 
