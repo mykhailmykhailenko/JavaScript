4f61776ab738ab2e79e1e50daa7592122385f7d3
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-10-17 13:20:58 +0300

    Array desctruction and other species

--- src/index.js
+++ src/index.js
@@ -1,32 +1,51 @@
 'use strict'
 
-const {brigthness, resolution, ...restOfObject} = monitor;
+const arr = [1, 2, 3, 4, 5, 6];
 
+const [firstElem, secondElem,,,fiveElement] = arr;
 
-function consoleValue({target: {value, name}}){
-//    const {target: {value}} = event;
+const [first, ...restOfArray] = arr;
+
+function getStringOfCortege([first, second]) {
+    return `${first} ${second}`
 }
 
-//event{
-//    target:{
-//        value,name
-//    }
-// }
 
 
-const userObj = {
+/*
+перетворити меп у ['1 - johndoe', '2 - janedoe', '3 - alexdoe']
+const map = new Map([[1, {username: 'john doe'}], [2, {username: 'janedoe'}], [3, {username: 'alexdoe'}]]);
+[...map.entries()].map((cortege)=>{
+    return `${cortege[0]} ${cortege[1].username}`
+}) 
+-----> 
+[...map.entries()].map(([key, {username}])=>{
+    return `${key} ${username}`
+})
+*/
+
+
+
+const settings = {
+    width: 200,
+    height: 400
+}
+
+function getSet({width, height, on = false}){
+    console.log(`${width} ${height} ${on}`)
+}
+
+const us = {
     name: {
-        first: 'John',
-        last: 'Doe',
-        fatherName: 'Petrovich'
+        first: 'Ricky'
     },
-    age: 20,
-    auth: {
-        email: 'doe@mail.com',
-        pass: 'qwerty'
-    }
-};
-
-function getFullName({name: {first, last, ...restOfName}, ...restOfUser}) {
-    return `${first} ${last}`
+    pass: '1234'
 }
+
+
+const {
+    name: {
+        first: userFirstName, 
+        last: userLastName = 'Anonumus'
+    }, 
+    pass = 'qwerty'} = us;
